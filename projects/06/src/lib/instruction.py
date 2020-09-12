9f5479c7a48810a73fa56a812940de220eebfe89
mtx2d <matrix2d@outlook.com>
2020-09-12 02:23:11 -0400

    add __repr__ for all types of instructions

--- projects/06/src/lib/instruction.py
+++ projects/06/src/lib/instruction.py
@@ -6,14 +6,28 @@ class AInstruction(Instruction):
     def __init__(self, value):
         self.value = value
 
+    def __repr__(self):
+        return "@" + self.value
+
 
 class CInstruction(Instruction):
-    def __init__(self, dest, comp, jump):
-        self.dest = dest
-        self.comp = comp
-        self.jump = jump
+    def __init__(self, dest: str, comp: str, jump: str):
+        self.dest: str = dest
+        self.comp: str = comp
+        self.jump: str = jump
+
+    def __repr__(self):
+        processed_cmd = self.comp
+        if self.dest:
+            processed_cmd = self.dest + "=" + processed_cmd
+        if self.jump:
+            processed_cmd = processed_cmd + ";" + self.jump
+        return processed_cmd
 
 
 class Label(Instruction):
     def __init__(self, name):
         self.name = name
+    
+    def __repr__(self):
+        return "@" + self.name
