3c113a80cbc52d4b9b41ed8bdf6ca2ad69af1feb
mtx2d <matrix2d@outlook.com>
2020-09-12 17:38:29 -0400

    move things around

--- projects/06/src/assembler.py
+++ projects/06/src/assembler.py
@@ -1,23 +1,26 @@
 from os import path
+from sys import argv
 
-from parser import Parser
+from lib.parser import Parser
 from lib.symbol_table import SymbolTable
 from lib.encoder import Encoder
 
 
 def main():
-    parser = Parser(file_path="some path here")
+    parser = Parser(path=argv[1])
     symbol_table = SymbolTable()
     encoder = Encoder()
 
-    with open("/output.hack", "w") as of:
+    with open("./output.hack", "w") as of:
         # First pass, prepares symbol table
         for inst in parser.get_instruction():
-            symbol_table
             pass
 
         # Second pass, generate machine code because address are all ready.
         for inst in parser.get_instruction():
             machine_code = encoder.encode(inst)
             of.write(machine_code)
-            pass
+
+
+if __name__ == '__main__':
+    main()
