04c273f543292ae0860e96b15a018317c4430689
mtx2d <matrix2d@outlook.com>
2020-09-13 15:25:41 -0400

    add test

--- projects/06/src/test_assembler.py
+++ projects/06/src/test_assembler.py
@@ -1,5 +1,8 @@
 import unittest
 import assembler
+import tempfile
+import filecmp
+import os
 
 TEST_ASM_FILES = [
     "../add/Add.asm",
@@ -11,6 +14,8 @@ TEST_ASM_FILES = [
     "../rect/RectL.asm",
 ]
 
+TEST_EXP_HACK_FILES = ["../add/Add/Expected_Add.hack"]
+
 
 class TestAssembler(unittest.TestCase):
     def test_main(self):
@@ -21,3 +26,17 @@ class TestAssembler(unittest.TestCase):
         args = assembler.parse_args("assembler.py in.asm out.hack".split())
         self.assertEqual(args.input, "in.asm")
         self.assertEqual(args.output, "out.hack")
+
+    def test_assembling_add(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            output = os.path.join(tempdir, "output.hack")
+            assembler.main(
+                "assember.py {} {}".format(TEST_ASM_FILES[0], output).split()
+            )
+
+            self.assertTrue(
+                filecmp.cmp(output, TEST_EXP_HACK_FILES[0], shallow=False),
+                "File differ: {} differnt from ground truth {}".format(
+                    output, TEST_EXP_HACK_FILES[0]
+                ),
+            )
