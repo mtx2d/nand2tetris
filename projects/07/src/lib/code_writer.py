68da3e35352c65a1c0a2b56bad502765e9cbd1aa
mtx2d <matrix2d@outlook.com>
2020-09-25 00:30:30 -0400

    update the push logic

--- projects/07/src/lib/code_writer.py
+++ projects/07/src/lib/code_writer.py
@@ -19,15 +19,35 @@ class CodeWriter:
             f"@{inst.value}",
             "D=A", # D = value
             f"@{inst.segment.upper()}", # @SEGMENT
-            "A=D+A", # A = SEGMENT + value -> element addr
-            "D=M", # D = *addr
+            "A=D+M", # A = value + SEGMENT -> element addr
+            "A=M", # D = *addr
 
             "@SP",
             "A=M"
             "M=D", # *SP = *addr
-            
+
             "@SP",
             "M=M+1" # SP++
+        ])
+    
+    @staticmethod
+    def write_pop(inst: PopInstruction) -> str:
+        """
+        addr = segment + value;  SP--; *addr = *SP;
+        """
+        "\n".join([
+            "// " + inst.__repr__(),
+
+            f"@{inst.value}"
+            "D=A", # D = value
+            f"@{inst.segment.upper()}", # @SEGMENT
+            "A=D+A",  # A = value + SEGMENT -> element addr -> point to the element in memory
+            "D=A" # D = addr
+
+            "@SP",
+            "M=M-1", # SP-- 
+
+
         ])
 
     def write(self, inst: Instruction) -> str:
