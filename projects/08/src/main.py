c0be41839de3e95196a69e8b2bbfee8ac10ab0b2
mtx2d <matrix2d@outlook.com>
2020-11-05 20:48:48 -0500

    fmt

--- projects/08/src/main.py
+++ projects/08/src/main.py
@@ -16,19 +16,24 @@ def parse_args(argv: List[str]) -> argparse.Namespace:
 
 def get_files(input_path):
     def is_vm_source_file(file_path):
-        return file_path.endswith(".vm") and os.path.isfile(os.path.join(input_path, file_path))
+        return file_path.endswith(".vm") and os.path.isfile(
+            os.path.join(input_path, file_path)
+        )
 
-    ans = [os.path.join(input_path, p) for p in os.listdir(input_path) if is_vm_source_file(p)]
+    ans = [
+        os.path.join(input_path, p)
+        for p in os.listdir(input_path)
+        if is_vm_source_file(p)
+    ]
     return ans
+
 
 def main(argv: List[str]) -> int:
     args = parse_args(argv)
 
     for file_path in get_files(args.input_path):
         parser = Parser(file_path)
-        code_writer = CodeWriter(
-            file_path
-        )  # need input filename for static variables
+        code_writer = CodeWriter(file_path)  # need input filename for static variables
         with open(args.output, "a") as of:
             print(code_writer.write_init(), file=of)
             for inst in parser.parse():
