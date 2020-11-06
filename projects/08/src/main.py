dd81b1d4b9fbca70122645b1450cbfc9732c96c0
mtx2d <matrix2d@outlook.com>
2020-11-05 20:37:40 -0500

    add logic to handle folder input

--- projects/08/src/main.py
+++ projects/08/src/main.py
@@ -10,20 +10,29 @@ from lib.code_writer import CodeWriter
 def parse_args(argv: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("input_path", help="input_path file")
-    parser.add_argument("output", help="output file")
+    parser.add_argument("output", default="./output.asm", help="output file")
     return parser.parse_args(argv[1:])
 
 
+def get_files(input_path):
+    def is_vm_source_file(file_path):
+        return file_path.endswith(".vm") and os.path.isfile(os.path.join(input_path, file_path))
+
+    ans = [os.path.join(input_path, p) for p in os.listdir(input_path) if is_vm_source_file(p)]
+    return ans
+
 def main(argv: List[str]) -> int:
     args = parse_args(argv)
-    parser = Parser(args.input_path)
-    code_writer = CodeWriter(
-        args.input_path
-    )  # need input filename for static variables
-    with open(args.output, "w") as of:
-        print(code_writer.write_init(), file=of)
-        for inst in parser.parse():
-            print(code_writer.write(inst), file=of)
+
+    for file_path in get_files(args.input_path):
+        parser = Parser(file_path)
+        code_writer = CodeWriter(
+            file_path
+        )  # need input filename for static variables
+        with open(args.output, "a") as of:
+            print(code_writer.write_init(), file=of)
+            for inst in parser.parse():
+                print(code_writer.write(inst), file=of)
 
     return 0
 
