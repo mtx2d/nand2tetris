c703621bbbef9c235b214f066bf9b195da5c4430
mtx2d <matrix2d@outlook.com>
2020-11-14 16:24:28 -0500

    WIP: implement jack grammar

--- projects/10/syntax_analyzer/lib/compilation_engine.py
+++ projects/10/syntax_analyzer/lib/compilation_engine.py
@@ -18,33 +18,44 @@ class CompilationEngine:
             raise TypeError(f"{token} should be either a Keyword or an Identifier.")
 
     @staticmethod
-    def compile_parameter_list(tokens, output_file):
-        pass
+    def compile_parameter_list(tokens, output_file, lvl = 0):
+       CompilationEngine.compile_type(tokens, output_file, lvl + 1) 
+       CompilationEngine.compile_var_name(tokens, output_file, lvl + 1)
 
     @staticmethod
-    def compile_subroutine_body(tokens, output_file):
-        pass
+    def compile_subroutine_body(tokens, output_file, lvl = 0):
+        output_file.write(next(tokens).to_xml(lvl + 1))
+        CompilationEngine.compile_var_dec(tokens, output_file, lvl + 1)
+        CompilationEngine.compile_statements(tokens, output_file, lvl + 1)
+        output_file.write(next(tokens).to_xml(lvl + 1))
+        
 
     @staticmethod
-    def compile_subroutine_dec(tokens, output_file):
+    def compile_subroutine_dec(tokens, output_file, lvl=0):
         subroutine_type = next(tokens)
-        return_type = CompilationEngine.compile_type(tokens, output_file)
+        output_file.write(subroutine_type.to_xml(lvl + 1))
+        CompilationEngine.compile_type(tokens, output_file)
         subroutine_name = next(tokens)
+        output_file.write(subroutine_name.to_xml(lvl + 1))
         left_bracket = next(tokens)
-        parameter_list = CompilationEngine.compile_parameter_list(tokens, output_file)
+        output_file.write(left_bracket.to_xml(lvl + 1))
+        CompilationEngine.compile_parameter_list(tokens, output_file, lvl+ 1)
         right_bracket = next(tokens)
-        subroutine_body = CompilationEngine.compile_subroutine_body(tokens, output_file)
+        output_file.write(right_bracket.to_xml(lvl + 1))
+        CompilationEngine.compile_subroutine_body(tokens, output_file, lvl + 1)
 
     @staticmethod
     def compile_class(tokens, output_file, lvl=0):
         kls = next(tokens)
-        print(kls.to_xml(lvl + 1), file=output_file)
+        output_file.write(kls.to_xml(lvl + 1))
         class_name = next(tokens)
-        print(class_name.to_xml(lvl + 1), file=output_file)
+        output_file.write(class_name.to_xml(lvl + 1))
         left = next(tokens)
+        output_file.write(left.to_xml(lvl + 1))
         CompilationEngine.compile_class_var_dec(tokens, output_file)
         CompilationEngine.compile_subroutine_dec(tokens, output_file)
         right = next(tokens)
+        output_file.write(right.to_xml(lvl + 1))
 
     @staticmethod
     def parse(tokens, output_file):
