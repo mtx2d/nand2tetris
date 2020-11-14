bd180ce322017ad2c1a4bf8245e9cfe23fc1e53d
mtx2d <matrix2d@outlook.com>
2020-11-14 12:07:00 -0500

    move files around

--- projects/10/syntax_analyzer/tests/test_syntax_analyzer.py
+++ projects/10/syntax_analyzer/tests/test_syntax_analyzer.py
@@ -11,17 +11,17 @@ class TestSyntaxAnalyzer(unittest.TestCase):
         self.assertRaises(FileNotFoundError, get_files, Path(NON_EXISTS_FILE_PATH))
 
     def test_get_files_for_non_jack_file(self):
-        NON_JACK_FILE_PATH = "../../ArrayTest/Main.xml"
+        NON_JACK_FILE_PATH = "../ArrayTest/Main.xml"
         self.assertRaises(Exception, get_files, Path(NON_JACK_FILE_PATH))
 
     def test_get_files_for_file(self):
-        FILE_PATH = "../../ArrayTest/Main.jack"
+        FILE_PATH = "../ArrayTest/Main.jack"
         abs_path = Path(__file__).joinpath(Path(FILE_PATH))
         expected = [abs_path]
         self.assertListEqual(expected, get_files(abs_path))
 
     def test_get_files_for_dir(self):
-        DIR_PATH = "../../ExpressionLessSquare"
+        DIR_PATH = "../ExpressionLessSquare"
         abs_dir_path = Path(__file__).joinpath(DIR_PATH)
         expected = [
             abs_dir_path.joinpath("Main.xml"),
