9d1601f3e5cac8583195ce60e36dc621a3849e37
mtx2d <matrix2d@outlook.com>
2020-11-15 19:05:52 -0500

    add mock tokens

--- projects/10/syntax_analyzer/tests/test_compilation_engine.py
+++ projects/10/syntax_analyzer/tests/test_compilation_engine.py
@@ -1,6 +1,31 @@
 import unittest
+from unittest import mock
+from more_itertools import peekable
+from lib.jack_token import Keyword, Symbol, Identifier, IntegerConstant, StringConstant
+from lib.compilation_engine import CompilationEngine
 
 
 class TestCompilationEngine(unittest.TestCase):
     def test_compile_class(self):
-        pass
+        mock_tokens = peekable(
+            iter(
+                [
+                    Keyword("if"),
+                    Symbol("("),
+                    Identifier("x"),
+                    Symbol("<"),
+                    IntegerConstant("153"),
+                    Symbol(")"),
+                    Symbol("{"),
+                    Keyword("let"),
+                    Identifier("city"),
+                    Symbol("="),
+                    StringConstant("Paris"),
+                    Symbol(";"),
+                    Symbol("}"),
+                ]
+            )
+        )
+        mock_output_file = mock.Mock()
+        CompilationEngine.compile_class(mock_tokens, mock_output_file)
+        self.assertTrue(False)
