0358416cd5cb0e41277513fe3d6e0821c85112b2
mtx2d <matrix2d@outlook.com>
2020-11-30 11:52:13 -0500

    implement symbol table main logic

--- projects/11/jack_compiler/lib/symbol_table.py
+++ projects/11/jack_compiler/lib/symbol_table.py
@@ -1,24 +1,5 @@
+from collections import defaultdict
 from typing import DefaultDict, Dict
-
-
-class Kind:
-    pass
-
-
-class Static(Kind):
-    pass
-
-
-class Field(Kind):
-    pass
-
-
-class Arg(Kind):
-    pass
-
-
-class Var(Kind):
-    pass
 
 
 class SymbolTable:
@@ -26,32 +7,50 @@ class SymbolTable:
         def __init__(self, name, type, kind, index):
             self.name: str = name
             self.type: str = type
-            self.kind: Kind = kind
+            self.kind: str = kind
             self.index: int = index
 
     def __init__(self):
         self.global_table: Dict[str, SymbolTable.Entry] = {}
         self.subroutine_table: Dict[str, SymbolTable.Entry] = {}
-        self.var_count: Dict[Kind, int] = DefaultDict{}
+        self.var_count: Dict[str, int] = defaultdict(int)
 
     def start_subroutine(self):
-        pass
+        self.subroutine_table.clear()
 
-    def define(self, name: str, type: str, kind: Kind):
-        if not isinstance(kind, Kind):
-            raise ValueError(f"invalid identifier kind: {kind}")
+    def define(self, name: str, type: str, kind: str):
         new_entry = SymbolTable.Entry(name, type, kind, self.var_count(kind) + 1)
-        self.global_table[name] =  new_entry
+        self.global_table[name] = new_entry
         self.var_count[kind] += 1
 
-    def var_count(self, kind: Kind) -> int:
-        pass
+    def var_count(self, kind: str) -> int:
+        if kind not in self.var_count:
+            raise ValueError(f"invalid kind: {kind}")
+        return self.var_count[kind]
 
-    def kind_of(self, name: str) -> Kind:
-        pass
+    def kind_of(self, name: str) -> str:
+        if name in self.subroutine_table:
+            return self.subroutine_table[name]
+        
+        if name in self.global_table:
+            return self.global_table[name]
+        
+        raise ValueError(f"name not found: {name}")
 
     def type_of(self, name: str) -> str:
-        pass
+        if name in self.subroutine_table:
+            return self.subroutine_table[name]
+        
+        if name in self.global_table:
+            return self.global_table[name]
+        
+        raise ValueError(f"name not found: {name}")
 
     def index_of(self, name: str) -> int:
-        pass
+        if name in self.subroutine_table:
+            return self.subroutine_table[name].index
+        
+        if name in self.global_table:
+            return self.global_table[name].index
+
+        raise ValueError(f"name not found: {name}")
