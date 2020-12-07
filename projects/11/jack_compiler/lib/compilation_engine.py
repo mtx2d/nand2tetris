741ce8f03d4b053979126eba39d19bb6e1d876c8
mtx2d <matrix2d@outlook.com>
2020-12-06 22:05:09 -0500

    add tokens for Seven.vm

--- projects/11/jack_compiler/lib/compilation_engine.py
+++ projects/11/jack_compiler/lib/compilation_engine.py
@@ -326,7 +326,7 @@ class CompilationEngine:
             symbol_table.define(name, type, "argument")
 
     def compile_subroutine_dec(self, tokens, symbol_table, lvl=0):
-        #TODO: handle constructor and method
+        # TODO: handle constructor and method
         symbol_table.start_subroutine()
         self.sub_routine_kind = next(tokens).val  # (constructor | function | method)
         self.sub_routine_return_type = next(tokens).val  # void | int | String
